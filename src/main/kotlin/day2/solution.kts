d9fde7a40005f6dc081ebc0a777e15393d831e77
Blixthalka <emil837@gmail.com>
2023-12-04 16:25:37 +0100

    wash

--- src/main/kotlin/day2/solution.kts
+++ src/main/kotlin/day2/solution.kts
@@ -2,63 +2,43 @@ package day2
 
 import Helpers.readFileByLine
 
-val maxi = mapOf(
+val maxBalls = mapOf(
     Pair("red", 12),
     Pair("green", 13),
     Pair("blue", 14)
 )
 
-val solution1 = readFileByLine("./data1.txt")
+val data = readFileByLine("./data1.txt")
     .map { it.replace(";", ",") }
-    .map {
-        val splitted = it.split(":")
+    .map { line ->
+        val splitted = line.split(":")
         val gameNr = splitted[0].replace("Game ", "").toInt()
-        val values = splitted[1].trim().split(",")
-            .map {
-                val show = it.trim().split(" ")
+        val gameValues = splitted[1].trim().split(",")
+            .map { game ->
+                val show = game.trim().split(" ")
                 Pair(show[0].toInt(), show[1])
             }
-        Pair(gameNr, values)
+        Pair(gameNr, gameValues)
     }
-    .filter { value -> !value.second.any { maxi[it.second]!! < it.first } }
-    .fold(0) { acc, value ->  acc + value.first }
+
+val solution1 = data
+    .filter { (_, gameValues) -> !gameValues.any { (pickedBalls, color) -> maxBalls[color]!! < pickedBalls } }
+    .fold(0) { acc, (gameNr, _) -> acc + gameNr }
 
 println("Solution 2: $solution1")
 
-val solution2 = readFileByLine("./data1.txt")
-    .map {
-        val splitted = it.split(":")
-        val gameNr = splitted[0].replace("Game ", "").toInt()
-        val values = splitted[1].trim().split(";")
-            .map {
-               value -> value.split(",")
-                   .map {
-                       val show = it.trim().split(" ")
-                       Pair(show[0].toInt(), show[1])
-                   }
+val solution2 = data
+    .map { (_, gameValues) -> gameValues }
+    .map { gameValues ->
+        gameValues.fold(mutableMapOf<String, Int>()) { gameMax, (pickedBalls, color) ->
+            if ((gameMax[color] ?: 0) < pickedBalls) {
+                gameMax[color] = pickedBalls
             }
-        Pair(gameNr, values)
-    }
-    .map { game ->
-        val gameMax = mutableMapOf<String, Int>()
-        game.second.forEach { section -> section.forEach {
-            var maxValue = 0;
-            if (gameMax.containsKey(it.second)) {
-                val currentMax = gameMax[it.second]
-                if (currentMax!! > it.first) {
-                    maxValue = currentMax
-                } else {
-                    maxValue = it.first
-                }
-            } else {
-                maxValue = it.first
-            }
-            gameMax[it.second] = maxValue
-        } }
-        gameMax
+            gameMax
+        }
     }
     .map { it["green"]!! * it["blue"]!! * it["red"]!! }
-    .fold(0) { acc, value ->  acc + value }
+    .fold(0) { acc, value -> acc + value }
 
 println("Solution 2: $solution2")
 
